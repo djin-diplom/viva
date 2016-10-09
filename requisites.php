ef9d776393648c0687943ff3379e0680a55b7290
21vek <git@github.com>
2016-10-09 13:50:02 +0300

    1

--- requisites.php
+++ requisites.php
@@ -1,5 +1,5 @@
 <?php
-$site_name = "vivadiplom.xyz";//"Viva.16mb.com";
-$site_url = "dfd";
-$email = "zakaz@vivadiplom.xyz";//"zakaz@viva.16mb.com";
+$site_name = "Viva.16mb.com";
+$site_url = "Viva.16mb.com";
+$email = "zakaz@viva.16mb.com";
 $adres = "г. Москва, ул. Марии Ульяновой, д. 17а, оф. 234-236.";
