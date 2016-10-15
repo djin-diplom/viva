3d4f0815df3fda422d9d00edddd5c32aa47f4f28
21vek <git@github.com>
2016-10-15 20:33:37 +0300

    1

--- consult.php
+++ consult.php
@@ -49,10 +49,73 @@ require("header.php");
 	div.responses div.one > div.rite > div > div.caption { text-shadow: 0 1px 0 white; margin-bottom: 5px; padding-bottom: 5px; border-bottom: 1px dotted rgba(196,85,39,0.5);; }
 	div.responses div.one > div.rite > div > div.caption i { font-size: 14px; color: #666; padding-left: 5px; }
 	div.responses div.one > div.rite > div > div.text { font-size: 14px; }
-	
+
+	 form#request_form { width: 75%; }
+	form#request_form table { width: 100%; }
+	form#request_form table tr td:first-of-type { white-space: nowrap; width: 10%; }
+	form#request_form table td { padding: 10px 20px 10px 0; font-size: 16px; white-space: nowrap; }
+	form#request_form table td input, form#request_form table td textarea, , form#request_form table td select { width: 100%; font-size: 16px; padding: 0.3em; }
+	form#request_form table td sup { font-size: 10px; color: #b00; }
+	form#request_form table td button { font-size: 20px; padding: 5px 10px; }
+	form#request_form table tr:last-of-type td input { visibility: hidden; width: 30px; }
+
 	
 </style>
-<button onclick="self.location=&#39;?add&#39;;" style="font-size: 18px;">Оставить свой отзыв</button><div class="responses">	<div class="one">
+<?php
+if(!empty($_GET['add']))
+{ ?>
+	<div id="request_div"><form id="request_form" method="post" enctype="multipart/form-data" action="consult.php">
+  <div style="display:none"><input type="hidden" name="csrfmiddlewaretoken" value="oBlyEDpYaBC7PNlqaGEbTOT3xFrlrCTL"></div><table class="order">
+    <tbody>
+	<tr>
+          <td class="field">
+Ваше имя
+<span style="color: red;">*</span>
+          </td>
+          <td>
+            <input style="width: 50%;" type="text" name="name" id="id_name">
+
+
+          </td>
+        </tr>
+
+	<tr>
+          <td class="field">
+Тема работы
+<span style="color: red;">*</span>
+          </td>
+          <td>
+            <textarea id="id_subject" style="width: 80%;" rows="3" cols="10" name="tema"></textarea>
+
+
+          </td>
+        </tr>
+	<tr>
+		<td class="field">&nbsp;</td>
+		<td><span style="color: red;">*</span> — обязательные поля</td>
+	</tr>
+	<tr>
+		<td class="field">&nbsp;</td>
+		<td>
+			<button type="submit" name = "submit" class="blue" style="font-size: 18px;">
+				Отправить отзыв на модерацию
+			</button>
+		</td>
+	</tr>
+</tbody>
+  </table>
+</form>
+
+<?php
+} else {?>
+
+<button onclick="self.location=&#39;?add=1&#39;;" style="font-size: 18px;">Оставить свой отзыв</button>
+<?php
+}
+
+?>
+
+			<div class="responses">	<div class="one">
 		<div class="left"><img src="./consult_files/user.png" width="32" height="32" alt=""></div>
 		<div class="rite">
 			<span></span>
