c99dcfeaa3ad0349841b9c930e3976488aef6b22
21vek <git@github.com>
2016-10-22 06:56:48 +0300

    1

--- kontakty.php
+++ kontakty.php
@@ -39,8 +39,7 @@ require("header.php");
 
 <h3><span style="color: maroon;">Расположение офиса</span></h3>Адрес - <?php echo $adres; ?><br>Мы находимся в самом центре Москвы - недалеко от метро Проспект Вернадского.<div>При себе необходимо иметь документ, в бизнес-центре действует пропускной режим.<br>
 <br>
-<img src="./index_files/mapdj.jpg" alt=""><br>
-<div class="marked"><a href="http://www.moscowmap.ru/imap_moscow.shtml#lat=55.68809088681727&lng=37.53355085849762&zoom=17&dom=71090">Открыть интерактивную карту</a></div>
+<?php echo $url_adres_yandex_frame; ?>
 <div class="marked">Фотографии бизнес-центра и офиса</div>
 
 <a href="index_files/2.jpg" onclick="return showImage(&#39;./index_files/2.jpg&#39;);"><dfn title="index_files/2.jpg"></dfn><img src="./index_files/2.jpg" width="140" height="140" alt=""></a>&nbsp;<a href="./index_files/3.jpg" onclick="return showImage(&#39;./index_files/3.jpg&#39;);"><dfn title="./index_files/3.jpg"></dfn><img src="./index_files/3.jpg" width="140" height="140" alt=""></a>&nbsp;<a href="./index_files/4.jpg" onclick="return showImage(&#39;./index_files/4.jpg&#39;);"><dfn title="./index_files/4.jpg"></dfn><img src="./index_files/4.jpg" width="140" height="140" alt=""></a></div>		</div>
--- requisites.php
+++ requisites.php
@@ -4,3 +4,4 @@ $site_url = "Vivadiplom.ru";
 $email = "zakaz@vivadiplom.ru";
 $email2 = "vivadiplom.ru@gmail.com";
 $adres = "г. Москва, ул. Марии Ульяновой, д. 17а, оф. 234-236.";
+$url_adres_yandex_frame = "<iframe src=\"https://api-maps.yandex.ru/frame/v1/-/CZggVH1z\" width=\"560\" height=\"400\" frameborder=\"0\"></iframe>";
