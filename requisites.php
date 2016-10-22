bb1a1e3ae68c4add9ca25148711d8be1064ea541
21vek <git@github.com>
2016-10-22 07:53:54 +0300

    1

--- requisites.php
+++ requisites.php
@@ -1,8 +1,8 @@
 <?php
-$site_name = "viva.hol.es";
-$site_url = "viva.hol.es ";
+$site_name = "Viva.hol.es";
+$site_url = "viva.hol.es";
 $email = "zakaz@viva.hol.es";
-$email2 = "vivahol@gmail.com";
+$email2 = "vivaholes@gmail.com";
 $email3 = "info@viva.hol.es";
 $adres = "г. Москва";
 $url_adres_yandex_frame = "";
