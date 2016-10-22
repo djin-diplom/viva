0178431f55b146ae320229690a869e8d230aba7b
21vek <git@github.com>
2016-10-22 07:00:23 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,5 +3,6 @@ $site_name = "<a href='http://vivadiplom.ru'>Да здравствует Дип�
 $site_url = "Vivadiplom.ru";
 $email = "zakaz@vivadiplom.ru";
 $email2 = "vivadiplom.ru@gmail.com";
+$email3 = "info@vivadiplom.ru";
 $adres = "г. Москва, ул. Марии Ульяновой, д. 17а, оф. 234-236.";
 $url_adres_yandex_frame = "<iframe src=\"https://api-maps.yandex.ru/frame/v1/-/CZggVH1z\" width=\"560\" height=\"400\" frameborder=\"0\"></iframe>";
