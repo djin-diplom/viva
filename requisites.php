20dfc406a0146d0d1314fccd6d3b1f1bec8fc0c0
21vek <git@github.com>
2016-10-22 07:46:17 +0300

    1

--- requisites.php
+++ requisites.php
@@ -1,8 +1,8 @@
 <?php
-$site_name = "<a href='http://vivadiplom.ru'>Да здравствует Диплом!</a>";
-$site_url = "Vivadiplom.ru";
-$email = "zakaz@vivadiplom.ru";
-$email2 = "vivadiplom.ru@gmail.com";
-$email3 = "info@vivadiplom.ru";
-$adres = "г. Москва, ул. Марии Ульяновой, д. 17а, оф. 234-236.";
-$url_adres_yandex_frame = "<iframe src=\"https://api-maps.yandex.ru/frame/v1/-/CZggVH1z\" width=\"560\" height=\"400\" frameborder=\"0\"></iframe>";
+$site_name = "viva.hol.es";
+$site_url = "viva.hol.es ";
+$email = "zakaz@viva.hol.es";
+$email2 = "vivahol@gmail.com";
+$email3 = "info@viva.hol.es";
+$adres = "г. Москва";
+$url_adres_yandex_frame = "";
