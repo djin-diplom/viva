521617c7266bde0db73280a85fc037faaba5c3bb
21vek <git@github.com>
2016-10-31 08:09:40 +0300

    1

--- vivadiplomfirstallemail/email.php
+++ vivadiplomfirstallemail/email.php
@@ -386,7 +386,7 @@ $path_img = $site_url."vivadiplomfirstallemail/";
 						<tbody><tr>
 							<td style="width:209px;height:129px;padding:0;overflow:visible">
 								<a href="<?php echo $site_url; ?>aktcii_i_skidki.php" class="m_3689207491995373867gp-button" target="_blank" ">
-									<img alt="android phones" src="<?php echo $path_img; ?>index_files/offer1.jpg" class="CToWUd" width: 400;>
+									<img alt="android phones" src="../index_files/offer1.jpg" class="CToWUd" width: 400;>
 								</a>
 							</td>
 							
