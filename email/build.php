da0f17e66d5762e54187a0f7ef4ad363b3557da3
21vek <git@github.com>
2016-11-28 08:45:04 +0300

    1

---
+++ email/build.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<?php
+
+require("../requisites.php");
+header('Content-Type: text/html; charset=utf-8');
+setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian');
+
+
+
+
+?>
+<html lang="ru">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- So that mobile will display zoomed in -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- enable media queries for windows phone 8 -->
+  <meta name="format-detection" content="telephone=no"> <!-- disable auto telephone linking in iOS -->
+  <title>Пиcьмо для будущих Заказчиков компaнии "<? echo $site_name; ?>"</title>
+
+  <style type="text/css">
+    body {
+      margin: 0;
+      padding: 0;
+      -ms-text-size-adjust: 100%;
+      -webkit-text-size-adjust: 100%;
+    }
+
+    table {
+      border-spacing: 0;
+    }
+
+    table td {
+      border-collapse: collapse;
+    }
+
+    .ExternalClass {
+      width: 100%;
+    }
+
+    .ExternalClass,
+    .ExternalClass p,
+    .ExternalClass span,
+    .ExternalClass font,
+    .ExternalClass td,
+    .ExternalClass div {
+      line-height: 100%;
+    }
+
+    .ReadMsgBody {
+      width: 100%;
+      background-color: #ebebeb;
+    }
+
+    table {
+      mso-table-lspace: 0pt;
+      mso-table-rspace: 0pt;
+    }
+
+    img {
+      -ms-interpolation-mode: bicubic;
+    }
+
+    .yshortcuts a {
+      border-bottom: none !important;
+    }
+
+    @media screen and (max-width: 599px) {
+      .force-row,
+      .container {
+        width: 100% !important;
+        max-width: 100% !important;
+      }
+    }
+    @media screen and (max-width: 400px) {
+      .container-padding {
+        padding-left: 12px !important;
+        padding-right: 12px !important;
+      }
+    }
+    .ios-footer a {
+      color: #aaaaaa !important;
+      text-decoration: underline;
+    }
+
+    @media screen and (max-width: 599px) {
+      .col {
+        width: 100% !important;
+        border-top: 1px solid #eee;
+        padding-bottom: 0 !important;
+      }
+
+      .cols-wrapper {
+        padding-top: 18px;
+      }
+
+      .img-wrapper {
+        float: right;
+        max-width: 40% !important;
+        height: auto !important;
+        margin-left: 12px;
+      }
+
+      .subtitle {
+        margin-top: 0 !important;
+      }
+    }
+    @media screen and (max-width: 400px) {
+      .cols-wrapper {
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+      }
+
+      .content-wrapper {
+        padding-left: 12px !important;
+        padding-right: 12px !important;
+      }
+    }
+  </style>
+</head>
+
+<body style="margin:0; padding:0;" bgcolor="#F0F0F0" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+
+<!-- 100% background wrapper (grey background) -->
+<table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" bgcolor="#F0F0F0">
+  <tr>
+    <td align="center" valign="top" bgcolor="#F0F0F0" style="background-color: #F0F0F0;">
+
+      <br>
+
+      <!-- 600px container (white background) -->
+      <table border="0" width="600" cellpadding="0" cellspacing="0" class="container" style="width:600px;max-width:600px">
+        <tr>
+          <td class="container-padding header" align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:26px;font-weight:bold;padding-bottom:12px;color:#DF4726;padding-left:24px;padding-right:24px">
+            Продажа дипломных работ на заказ и другие услуги от компании "<?php echo $site_name; ?>"</span>
+            <br><span style="color: green;">Скидки, акции, новогоднее снижение цен!</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="content" align="left" style="padding-top:12px;padding-bottom:12px;background-color:#ffffff">
+
+            <table width="600" border="0" cellpadding="0" cellspacing="0" class="force-row" style="width: 600px;">
+              <tr>
+                <td class="content-wrapper" style="padding-left:24px;padding-right:24px">
+                  <br>
+                  <div class="title" style="font-family:Helvetica, Arial, sans-serif;font-size:18px;font-weight:600;color:#374550">Компaния "<a href=<?php echo $site_url; ?>><? echo $site_name; ?></a>" БЕЗ ПРЕДОПЛАТЫ пришлет вам часть работы для ознакомления, чтобы вы сделали выбор!<br> Просто сделайте заказ на сайте с помощью формы заказа!</div>
+                </td>
+              </tr>
+              <tr>
+                <td class="cols-wrapper" style="padding-left:12px;padding-right:12px">
+
+                  <!--[if mso]>
+                  <table border="0" width="576" cellpadding="0" cellspacing="0" style="width: 576px;">
+                    <tr><td width="192" style="width: 192px;" valign="top"><![endif]-->
+
+
+                  <table width="192" border="0" cellpadding="0" cellspacing="0" align="left" class="force-row" style="width: 192px;">
+                    <tr>
+                      <td class="col" valign="top" style="padding-left:12px;padding-right:12px;padding-top:18px;padding-bottom:12px">
+                        <table border="0" cellpadding="0" cellspacing="0" class="img-wrapper">
+                          <tr>
+                            <td style="padding-bottom:18px"><img src='<?php echo $site_url."/index_files/slider2.jpg"; ?>' border="0" alt="aкция Три Цeны" width="168" height="110" hspace="0" vspace="0" style="max-width:100%; " class="image"></td>
+                          </tr>
+                        </table>
+                        <table border="0" cellpadding="0" cellspacing="0">
+                          <tr>
+                            <td class="subtitle" style="font-family:Helvetica, Arial, sans-serif;font-size:16px;line-height:22px;font-weight:600;color:#2469A0;padding-bottom:6px">Курсовая</td>
+                          </tr>
+                        </table>
+                        <div class="col-copy" style="font-family:Helvetica, Arial, sans-serif;font-size:13px;line-height:20px;text-align:left;color:#333333">От 1500 рублей<br>
+                          Защита диплома и сама дипломная работа - это итог и венец нескольких лет студенчества. Это то, к чему вы шли так долго, и ваш пропуск в успешную и действительно взрослую жизнь. Но что делать, если защита все ближе, а вы понимаете, что не готовы к ней на том уровне, какого вам бы хотелось? Это сейчас, увы, случай нередкий, и дело не в лени или безалаберности.
+                        </div>
+                        <br>
+                      </td>
+                    </tr>
+                  </table>
+
+
+                  <!--[if mso]></td><td width="192" style="width: 192px;" valign="top"><![endif]-->
+
+
+                  <table width="192" border="0" cellpadding="0" cellspacing="0" align="left" class="force-row" style="width: 192px;">
+                    <tr>
+                      <td class="col" valign="top" style="padding-left:12px;padding-right:12px;padding-top:18px;padding-bottom:12px">
+                        <table border="0" cellpadding="0" cellspacing="0" class="img-wrapper">
+                          <tr>
+                            <td style="padding-bottom:18px"><img src='<?php echo $site_url."/index_files/slider4.jpg"; ?>' width="168" height="110" hspace="0" vspace="0" style="max-width:100%; " class="image"></td>
+                          </tr>
+                        </table>
+                        <table border="0" cellpadding="0" cellspacing="0">
+                          <tr>
+                            <td class="subtitle" style="font-family:Helvetica, Arial, sans-serif;font-size:16px;line-height:22px;font-weight:600;color:#2469A0;padding-bottom:6px">Дипломная</td>
+                          </tr>
+                        </table>
+                        <div class="col-copy" style="font-family:Helvetica, Arial, sans-serif;font-size:13px;line-height:20px;text-align:left;color:#333333">От 4500 рублей<br>
+                          Вам, наверное, не раз доводилось слышать ворчанье преподавателей о том, что когда они были студентами, все было совершенно иначе. И это чистая правда. Однако это не значит, что учиться стало легче. В распоряжении современных студентов стационарные и портативные компьютеры, интернет, электронные библиотеки... И живут они в другом мире - жить стало дороже, зарабатывать на жизнь - сложнее, а выдерживать ее темп и все успевать - гораздо труднее. Знакомо?
+                        </div>
+                        <br>
+                      </td>
+                    </tr>
+                  </table>
+
+
+                  <!--[if mso]></td><td width="192" style="width: 192px;" valign="top"><![endif]-->
+
+
+                  <table width="192" border="0" cellpadding="0" cellspacing="0" align="left" class="force-row" style="width: 192px;">
+                    <tr>
+                      <td class="col" valign="top" style="padding-left:12px;padding-right:12px;padding-top:18px;padding-bottom:12px">
+                        <table border="0" cellpadding="0" cellspacing="0" class="img-wrapper">
+                          <tr>
+                            <td style="padding-bottom:18px"><img src='<?php echo $site_url."/index_files/slider3.jpg"; ?>' border="0" alt="Мы продaeм только yникaльныe рaботы по прaвилy Один Тeкcт В Одни Рyки" width="168" height="110" hspace="0" vspace="0" style="max-width:100%; " class="image"></td>
+                          </tr>
+                        </table>
+                        <table border="0" cellpadding="0" cellspacing="0">
+                          <tr>
+                            <td class="subtitle" style="font-family:Helvetica, Arial, sans-serif;font-size:16px;line-height:22px;font-weight:600;color:#2469A0;padding-bottom:6px">Магистерская</td>
+                          </tr>
+                        </table>
+                        <div class="col-copy" style="font-family:Helvetica, Arial, sans-serif;font-size:13px;line-height:20px;text-align:left;color:#333333">От 6000 рублей<br>
+                          Одно из направлений деятельности компании - это продажа дипломных работ практически по всем дисциплинам. У нас трудятся выпускники ведущих вузов страны, которые пишут работы самостоятельно по утвержденному плану с гарантией уникальности и адекватности теме, которую вы выбрали совместно с научным руководителем.
+                        </div>
+                        <br>
+                      </td>
+                    </tr>
+                  </table>
+
+
+                  <!--[if mso]></td></tr></table><![endif]-->
+
+                </td>
+              </tr>
+              <tr>
+                <td class="container-padding header" align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:24px;font-weight:bold;padding-bottom:12px;color:#DF4726;padding-left:24px;padding-right:24px">
+                  <a href=<?php echo $site_url."order.php"; ?>>Форма для заказа! Вам сюда!</a><br>
+                  Мы работаем по договору оферты и гарантируем выполнение заказов точно в срок. Просто сделайте заказ - и будьте спокойны и уверены в результате!
+                </td>
+              </tr>
+            </table>
+
+          </td>
+        </tr>
+        <tr>
+          <td class="container-padding footer-text" align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:12px;line-height:16px;color:#aaaaaa;padding-left:24px;padding-right:24px">
+            <br><br>
+
+
+            <strong><? echo $site_name; ?></strong><br>
+            Компания работает на рынке образовательных услуг уже более 10 лет. За этот немалый срок мы помогли множеству студентов: написали бесчисленное количество дипломов, научных докладов, диссертаций, курсовых, рефератов и других работ на заказ. Мы готовы взяться за дело даже в самых сложных случаях, например, когда работа должна быть выполнена в экстремально короткие сроки. Результат всегда оправдывает ожидания наших клиентов, и мы гордимся этим!
+            <br>
+            <span class="ios-footer">
+              <?php echo $adres; ?> <br>
+
+            </span>
+
+              <span class="ios-footer">
+              Если данное письмо пришло к вам по ошибке, вы можете отписаться <a href='<?php echo $site_url."otpiska.php"?>' >тут</a>.<br>
+            </span>
+
+            © 2007–2016 <a href=<?php echo $site_url; ?> style="color:#aaaaaa"><?php echo $site_url; ?></a><br>
+
+            <br><br>
+
+          </td>
+        </tr>
+      </table>
+      <!--/600px container -->
+
+
+    </td>
+  </tr>
+</table>
+<!--/100% background wrapper-->
+
+</body>
+</html>
