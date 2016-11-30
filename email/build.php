161c0486336f86d7d39ba14d2c92346d391efb97
21vek <git@github.com>
2016-11-30 21:13:41 +0300

    1

--- email/build.php
+++ email/build.php
@@ -15,7 +15,7 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
   <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- So that mobile will display zoomed in -->
   <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- enable media queries for windows phone 8 -->
   <meta name="format-detection" content="telephone=no"> <!-- disable auto telephone linking in iOS -->
-  <title>Специально для Студентов от образовательного центра "<? echo $site_name; ?>"</title>
+  <title>Эксклюзив для Студенчества от компании  "<? echo $site_name; ?>"</title>
 
   <style type="text/css">
     body {
@@ -131,9 +131,10 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
       <table border="0" width="600" cellpadding="0" cellspacing="0" class="container" style="width:600px;max-width:600px">
         <tr>
           <td class="container-padding header" align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:26px;font-weight:bold;padding-bottom:12px;color:#DF4726;padding-left:24px;padding-right:24px">
-            Пишем любые работы по всей России:	дипломы, курсовые, рефераты и прочее.<br>
-            Образовательный центр "<?php echo $site_name; ?>".
-            <br><span style="color: green;">Акции (20%), падение цен в декабре!</span>
+            СКОЛЬКО СТОИТ ДИПЛОМНАЯ РАБОТА,<br>КАК ЕЕ ЗАКАЗАТЬ НЕДОРОГО?
+            <br><span style="color: green;">Легко! Акции, падение цен в Новый Год!</span><br>
+            Все возможно в компании "<?php echo $site_name; ?>"!
+
           </td>
         </tr>
         <tr>
@@ -143,10 +144,10 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
               <tr>
                 <td class="content-wrapper" style="padding-left:24px;padding-right:24px">
                   <br>
-                  <div class="title" style="font-family:Helvetica, Arial, sans-serif;font-size:18px;font-weight:600;color:#374550">Образовательный центр "<a href=<?php echo $site_url; ?>><? echo $site_name; ?></a>"
-                    БЕЗ ПРЕДОПЛАТЫ
-                    предoставит Вам часть диплома, реферата, диссертации, чтобы вы ознакомились с работой и сделали выбoр!<br>
-                    Заполните форму на нашем портале, чтобы сделать заказ!</div>
+                  <div class="title" style="font-family:Helvetica, Arial, sans-serif;font-size:18px;font-weight:600;color:#374550">Учебная компания "<a href=<?php echo $site_url; ?>><? echo $site_name; ?></a>"
+                    БЕСПЛАТНО (не требуется предоплата!)
+                    папишет и пришлет Вам часть Вашей работы, сделайте верный выбор!<br>
+                    Для заказа работы на нашем сайте есть удобная форма!</div>
                 </td>
               </tr>
               <tr>
@@ -167,13 +168,14 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
                         </table>
                         <table border="0" cellpadding="0" cellspacing="0">
                           <tr>
-                            <td class="subtitle" style="font-family:Helvetica, Arial, sans-serif;font-size:16px;line-height:22px;font-weight:600;color:#2469A0;padding-bottom:6px">Курсовая</td>
+                            <td class="subtitle" style="font-family:Helvetica, Arial, sans-serif;font-size:16px;line-height:22px;font-weight:600;color:#2469A0;padding-bottom:6px">Курсoвая</td>
                           </tr>
                         </table>
-                        <div class="col-copy" style="font-family:Helvetica, Arial, sans-serif;font-size:13px;line-height:20px;text-align:left;color:#333333">От 1450 рублей<br>
-                          В 21-м веке, когда мы должны успевать одновременно работать, учиться, отдыхать и многое другое, чем-то иногда приходится жертвовать.
-                          Если вы решитесь написать диплом на заказ — это позволит не отвлекаться от повседневных дел и закончить ВУЗ достойно.<br>
-                          Заказ диплома — важное решение, поскольку Вы доверяете компании, в которой решили сделать решающий шаг на встречу аттестату.
+                        <div class="col-copy" style="font-family:Helvetica, Arial, sans-serif;font-size:13px;line-height:20px;text-align:left;color:#333333">От 1550 рублей<br>
+                          С каждым годом все большее число людей склоняется к мысли, что заказать написание дипломной работы
+                          у специалистов гораздо удобнее, чем писать ее самостоятельно. Дело в том, что дипломная работа - это
+                          серьезный научный труд, показывающий какие знания, умения и навыки студент приобрел за время учебы в ВУЗе.
+
                         </div>
                         <br>
                       </td>
@@ -194,14 +196,14 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
                         </table>
                         <table border="0" cellpadding="0" cellspacing="0">
                           <tr>
-                            <td class="subtitle" style="font-family:Helvetica, Arial, sans-serif;font-size:16px;line-height:22px;font-weight:600;color:#2469A0;padding-bottom:6px">Дипломная</td>
+                            <td class="subtitle" style="font-family:Helvetica, Arial, sans-serif;font-size:16px;line-height:22px;font-weight:600;color:#2469A0;padding-bottom:6px">Диплoмная</td>
                           </tr>
                         </table>
-                        <div class="col-copy" style="font-family:Helvetica, Arial, sans-serif;font-size:13px;line-height:20px;text-align:left;color:#333333">От 4400 рублей<br>
-                          Мы понимаем, насколько серьезно оценивается любой дипломный проект в ВУЗе.
-                          Именно поэтому мы со всей ответственностью подходим к выполнению работы и с каждым клиентом, с каждым дипломом работаем отдельно.
-                          Мы готовы разработать для каждого заказчика индивидуальный план:
-                          написать дипломную работу по частям, корректировать ее в ходе работы, устанавливать для каждой части свои сроки.
+                        <div class="col-copy" style="font-family:Helvetica, Arial, sans-serif;font-size:13px;line-height:20px;text-align:left;color:#333333">От 4560 рублей<br>
+                          Сложность проекта определяет его компетентность в рассматриваемом вопросе, а умение находить и реализовывать
+                          оригинальные решения в рамках поставленных
+                          научным руководителем задач демонстрирует аналитический склад ума и ответственный подход.
+                          Далеко не каждому удается написать дипломную работу самостоятельно, ведь это требует огромных затрат времени и сил.
                         </div>
                         <br>
                       </td>
@@ -225,14 +227,10 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
                             <td class="subtitle" style="font-family:Helvetica, Arial, sans-serif;font-size:16px;line-height:22px;font-weight:600;color:#2469A0;padding-bottom:6px">Магистерская</td>
                           </tr>
                         </table>
-                        <div class="col-copy" style="font-family:Helvetica, Arial, sans-serif;font-size:13px;line-height:20px;text-align:left;color:#333333">От 6500 рублей<br>
-                          Мы предлагаем Вам написать дипломную работу
-                          любой сложности
-                          на любую тему
-                          с различными требованиями Вашего преподавателя.
-                          Написание дипломов у нас происходит силами рук квалифицированных преподавателей.
-                          Оформить диплом на заказ у нас - значит доверить себя в надежные руки, так как каждая заказанная у нас работа
-                          оригинальна, выполнена по всем требованиям, оформлена по ГОСТу.
+                        <div class="col-copy" style="font-family:Helvetica, Arial, sans-serif;font-size:13px;line-height:20px;text-align:left;color:#333333">От 6600 рублей<br>
+                          Если вы хотите получить действительно профессиональную работу, за нее придется немало заплатить.
+                          Вопрос качества должен стоять превыше вопроса о том, сколько стоит дипломная работа, но в любом случае вы можете сэкономить до 50-60%
+                          от конечной стоимости диплома, воспользовавшись нашими услугами.
                         </div>
                         <br>
                       </td>
@@ -246,8 +244,8 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
               </tr>
               <tr>
                 <td class="container-padding header" align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:24px;font-weight:bold;padding-bottom:12px;color:#DF4726;padding-left:24px;padding-right:24px">
-                  <a href=<?php echo $site_url."order.php"; ?>>Фoрма заказа рабoты! Проследуйте на сайт!</a><br>
-                  Мы пишем работы по всем предметам!
+                  <a href=<?php echo $site_url."order.php"; ?>>Фoрма закaза нашего портала! Нажми!</a><br>
+                  Мы знаем 7 способов сэкономить на написании дипломной работы под заказ!
                 </td>
               </tr>
             </table>
@@ -261,11 +259,16 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
 
             <strong><? echo $site_name; ?></strong><br>
 
-            Вы решили заказать диплом у нас? Вы можете быть уверены: оригинальность работы будет не менее 75%,
-            что допустимо во всех ВУЗах. Все дипломы, написанные у нас, мы проверяем на процент плагиата и требуем
-            от преподавателя оригинальность от 75%. Тем не менее, каждый, кто заказал у нас дипломный проект, имеет
-            длительную гарантию четыре месяца, т. е. даже если Ваш преподаватель потребовал внести некоторые
-            изменения только через месяц или два (например), мы внесем коррективы бесплатно.
+            Сроки. Время, отведенное на написание дипломной работы, играет едва ли не главную роль - чем срочнее заказ, тем дороже
+            обойдутся услуги профессионалов. В идеале лучше делать заявку на диплом за 1-2 месяца до сдачи, чтобы автор мог не торопиться.
+            Дипломные работы, написать которые нужно всего за несколько дней, обычно стоят примерно в 3 раза дороже, чем несрочные.
+            Четкость задачи. Чем подробнее описаны требования, тем проще автору ориентироваться именно на вашу тему.
+            Если вы предоставите компании уже готовый список литературы или план дипломной работы, цена за научный труд существенно сократится.
+            Крупный заказ. Многие компании готовы сделать скидку до 15-20% от общей суммы, если вы единовременно закажете сразу несколько дипломных работ, например, для себя и своих одногруппников.
+            Акции и спецпредложения. Лучшие российские компании регулярно проводят акции и предлагают скидки на свои услуги. Вовремя воспользовавшись ограниченным предложением,
+            вы сможете купить диплом на заказ значительно дешевле его обычной стоимости.
+            Таким образом, чтобы сэкономить деньги не в ущерб качеству заказанного диплома, нужно максимально четко поставить задачи перед автором и по возможности упростить его работу. Сделав заказ заранее, вы сэкономите и деньги, и собственные нервы,
+            так как дипломная работа будет у вас на руках задолго до окончательного срока сдачи.
             <span class="ios-footer">
               <?php echo $adres; ?> <br>
 
