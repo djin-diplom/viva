c9c448b82516f011e7db54299d3ffe06bfb3b739
21vek <git@github.com>
2016-12-03 12:07:53 +0300

    1

---
+++ admin_vika.php
@@ -0,0 +1,12 @@
+<form action="kabinet/adm_panel.php" method="post" >
+    <input type="submit" value="Перейти на kabinet/adm_panel.php (Редактирование данных заказчиков)"><br>
+</form>
+<form action="kabinet/clients" method="post" >
+    <input type="submit" value="Перейти на kabinet/clients (Список текущих заказчиков)"><br>
+</form>
+<form action="basa_clients.php" method="post" >
+    <input type="submit" value="Перейти на basa_clients.php (Форма для создания письма оценки заказа)"><br>
+</form>
+<form action="excel/upload_file.php" method="post" >
+    <input type="submit" value="Перейти на excel/upload_file.php (Массовая рассылка заказчикам)"><br>
+</form>
