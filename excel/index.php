34ba59a292ef62192c92b67501aaebc1d981c1fa
21vek <git@github.com>
2016-12-04 16:32:16 +0300

    1

--- excel/index.php
+++ excel/index.php
@@ -11,7 +11,7 @@
 error_reporting(E_ALL);
 require("../requisites.php");
 require_once ("sender.php");
-$sender = new sender;
+$sender = new sender('zakaz@bazaznanij.xyz');
 
 if(!empty($_GET['send']))
 {
