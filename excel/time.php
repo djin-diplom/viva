6505b38495cacc936d0729e1191e7e81326cc17c
21vek <git@github.com>
2016-12-05 07:56:23 +0300

    1

--- excel/time.php
+++ excel/time.php
@@ -12,7 +12,7 @@ echo "Номер ".$i;
 <p>Ведется отправка <?php echo $i;?> письма.</p>
 <script>
     function func() {
-        alert( 'Привет' );
+        //alert( 'Привет' );
         return location.href = "send_email.php?nomer=" + <?php echo $i; ?> + "&filename=" + "<?php echo $filename; ?>";
     }
     setTimeout(func, 1000);
