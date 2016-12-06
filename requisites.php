c71eab5f0c4d30dfc50404808d6774fd50841c78
21vek <git@github.com>
2016-12-06 20:46:13 +0300

    1

--- requisites.php
+++ requisites.php
@@ -32,7 +32,7 @@ if ($per == 1) {
     $email_5 = "adt@bazaznanij.com";
     $email2 = "bazaznanij.com@gmail.com";
     $email3 = "info@bazaznanij.com";
-    $adres = "197022, г. Санкт-Петербург, Каменноостровский пр-кт, <br>дом № 27 литер Б, оф. 134-136, ИНН 7813239461";
+    $adres = "197022, г. Санкт-Петeрбург, Каменноостровcкий пр-кт, <br>дом № 27 литер Б, oф. 134-136, ИНН 7813239461";
     $url_adres_yandex = "https://yandex.ru/maps/-/CZs6uQyT";
     $url_adres_yandex_frame = "<iframe src=\"https://api-maps.yandex.ru/frame/v1/-/CZs6uQyT\" width=\"560\" height=\"400\" frameborder=\"0\"></iframe>";
     $url_oplata = "https://money.yandex.ru/to/410014714516309";
