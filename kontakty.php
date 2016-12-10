54d3eaf2a2e44093d4cd7b05bf0f22e9a3659cee
21vek <git@github.com>
2016-12-10 22:35:35 +0300

    1

--- kontakty.php
+++ kontakty.php
@@ -38,12 +38,27 @@ require("header.php");
 
 </p><h3><span style="color: maroon;">Часы работы</span></h3><p>С <b>6:00 до 22:00</b>; &nbsp;<span style="color: maroon;"><b>без выходных.</b></span><br></p><br>
 
-<h3><span style="color: maroon;">Расположение офиса</span></h3>Адрес - <?php echo $adres; ?><br>Мы находимся в самом центре города - недалеко от метро.<div>При себе необходимо иметь документ, в бизнес-центре действует пропускной режим.<br>
+<h3><span style="color: maroon;">Расположение офиса</span></h3>Адрес - <?php echo $adres; ?>
+			<?php if (!empty($town)) : ?>
+			<br>Мы находимся в самом центре города - недалеко от метро.<div>При себе необходимо
+				иметь документ, в бизнес-центре действует пропускной режим.<br>
+				<?php endif ?>
 <br>
 <?php echo $url_adres_yandex_frame; ?>
+				<?php if (!empty($town)) : ?>
 <div class="marked">Фотографии бизнес-центра и офиса</div>
 
-<a href="index_files/2.jpg" onclick="return showImage(&#39;./index_files/2.jpg&#39;);"><dfn title="index_files/2.jpg"></dfn><img src="./index_files/2.jpg" width="140" height="140" alt=""></a>&nbsp;<a href="./index_files/3.jpg" onclick="return showImage(&#39;./index_files/3.jpg&#39;);"><dfn title="./index_files/3.jpg"></dfn><img src="./index_files/3.jpg" width="140" height="140" alt=""></a>&nbsp;<a href="./index_files/4.jpg" onclick="return showImage(&#39;./index_files/4.jpg&#39;);"><dfn title="./index_files/4.jpg"></dfn><img src="./index_files/4.jpg" width="140" height="140" alt=""></a></div>		</div>
+<a href="index_files/2.jpg" onclick="return showImage(&#39;./index_files/2.jpg&#39;);">
+	<dfn title="index_files/2.jpg"></dfn><img src="./index_files/2.jpg" width="140" height="140" alt="">
+</a>&nbsp;<a href="./index_files/3.jpg" onclick="return showImage(&#39;./index_files/3.jpg&#39;);">
+					<dfn title="./index_files/3.jpg"></dfn><img src="./index_files/3.jpg" width="140" height="140" alt="">
+				</a>&nbsp;
+				<a href="./index_files/4.jpg" onclick="return showImage(&#39;./index_files/4.jpg&#39;);">
+					<dfn title="./index_files/4.jpg"></dfn><img src="./index_files/4.jpg" width="140" height="140" alt=""></a>
+			</div>
+		<?php endif ?>
+		</div>
+
 	</section>
 </div>
 
