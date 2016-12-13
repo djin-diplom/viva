bbb3f8f45ca87a7160beda4f3a182d917204354b
21vek <git@github.com>
2016-12-13 21:14:51 +0300

    1

--- requisites.php
+++ requisites.php
@@ -1,5 +1,152 @@
 <?php
 
+
+$main_name = "Knowbaza.ru";
+
+$url_town_piter = 'http://'.$main_name.'/';
+$url_town_moscow = 'http://moscow.'.$main_name.'/';
+$url_town_kazan = 'http://kazan.'.$main_name.'/';
+$url_town_novosibirsk = 'http://novosibirsk.'.$main_name.'/';
+$url_town_ufa = 'http://ufa.'.$main_name.'/';
+
+
+    $site_name = $main_name;
+    $password = '111qwaszx';
+    $site_url = "http://".$main_name."/";
+    $site_phone = "+7 (960) 590 95 72";
+    $email = "zakaz@".$main_name;
+    $email_1 = "advertisement@".$main_name;
+    $email_2 = "advertising@".$main_name;
+    $email_3 = "promotion@".$main_name;
+    $email_4 = "advt@".$main_name;
+    $email_5 = "adt@".$main_name;
+    $email_6 = "zakaz@".$main_name;
+    $email_7 = "info@".$main_name;
+    $email_8 = "inform@".$main_name;
+    $email_9 = "information@".$main_name;
+    $email_10 = "order@".$main_name;
+    $email_11 = "ad@".$main_name;
+    $email_12 = "publicity@".$main_name;
+    $email2 = $main_name."@gmail.com";
+    $email3 = "info@".$main_name;
+    $adres = "197022, г. Санкт-Петeрбург, Каменноостровcкий пр-кт, <br>дом № 27 литер Б, oф. 134-136, ИНН 7813239461";
+    $url_adres_yandex = "https://yandex.ru/maps/-/CZs6uQyT";
+    $url_adres_yandex_frame = "<iframe src=\"https://api-maps.yandex.ru/frame/v1/-/CZs6uQyT\" width=\"560\" height=\"400\" frameborder=\"0\"></iframe>";
+    $url_oplata = "https://money.yandex.ru/to/410014778658914";
+
+
+if (@$town == 'kazan') {
+    $main_name = "Kazan.".$main_name;
+    $site_name = $main_name;
+    $password = '111qwaszx';
+    $site_url = "http://".$main_name."/";
+    $site_phone = "+7 (960) 590 95 72";
+    $email = "zakaz@".$main_name;
+    $email_1 = "advertisement@".$main_name;
+    $email_2 = "advertising@".$main_name;
+    $email_3 = "promotion@".$main_name;
+    $email_4 = "advt@".$main_name;
+    $email_5 = "adt@".$main_name;
+    $email_6 = "zakaz@".$main_name;
+    $email_7 = "info@".$main_name;
+    $email_8 = "inform@".$main_name;
+    $email_9 = "information@".$main_name;
+    $email_10 = "order@".$main_name;
+    $email_11 = "ad@".$main_name;
+    $email_12 = "publicity@".$main_name;
+    $email2 = $main_name."@gmail.com";
+    $email3 = "info@".$main_name;
+    $adres = "420077, г. Казань, Софийская ул, <br>дом № 16, oф. 55, ИНН 7813239461";
+    $url_adres_yandex = "https://yandex.ru/maps/-/CZDxARyM";
+    $url_adres_yandex_frame = "<iframe src=\"https://api-maps.yandex.ru/frame/v1/-/CZDxARyM\" width=\"560\" height=\"400\" frameborder=\"0\"></iframe>";
+    $url_oplata = "https://money.yandex.ru/to/410014778658914";
+}
+
+if (@$town == 'moscow') {
+    $main_name = "Moscow.".$main_name;
+    $site_name = $main_name;
+    $password = '111qwaszx';
+    $site_url = "http://".$main_name."/";
+    $site_phone = "+7 (960) 590 95 72";
+    $email = "zakaz@".$main_name;
+    $email_1 = "advertisement@".$main_name;
+    $email_2 = "advertising@".$main_name;
+    $email_3 = "promotion@".$main_name;
+    $email_4 = "advt@".$main_name;
+    $email_5 = "adt@".$main_name;
+    $email_6 = "zakaz@".$main_name;
+    $email_7 = "info@".$main_name;
+    $email_8 = "inform@".$main_name;
+    $email_9 = "information@".$main_name;
+    $email_10 = "order@".$main_name;
+    $email_11 = "ad@".$main_name;
+    $email_12 = "publicity@".$main_name;
+    $email2 = $main_name."@gmail.com";
+    $email3 = "info@".$main_name;
+    $adres = "119311, г. Москва ул. Строителей, <br>дом № 11, oф. 234-236, ИНН 7813239461";
+    $url_adres_yandex = "https://yandex.ru/maps/-/CZDxEQMT";
+    $url_adres_yandex_frame = "<iframe src=\"https://api-maps.yandex.ru/frame/v1/-/CZDxEQMT\" width=\"560\" height=\"400\" frameborder=\"0\"></iframe>";
+    $url_oplata = "https://money.yandex.ru/to/410014778658914";
+}
+
+if (@$town == 'novosibirsk') {
+    $main_name = "Novosibirsk.".$main_name;
+    $site_name = $main_name;
+    $password = '111qwaszx';
+    $site_url = "http://".$main_name."/";
+    $site_phone = "+7 (960) 590 95 72";
+    $email = "zakaz@".$main_name;
+    $email_1 = "advertisement@".$main_name;
+    $email_2 = "advertising@".$main_name;
+    $email_3 = "promotion@".$main_name;
+    $email_4 = "advt@".$main_name;
+    $email_5 = "adt@".$main_name;
+    $email_6 = "zakaz@".$main_name;
+    $email_7 = "info@".$main_name;
+    $email_8 = "inform@".$main_name;
+    $email_9 = "information@".$main_name;
+    $email_10 = "order@".$main_name;
+    $email_11 = "ad@".$main_name;
+    $email_12 = "publicity@".$main_name;
+    $email2 = $main_name."@gmail.com";
+    $email3 = "info@".$main_name;
+    $adres = "630099, г. Новосибирск, Ленина ул, <br>дом № 15, oф. 153, ИНН 7813239461";
+    $url_adres_yandex = "https://yandex.ru/maps/-/CZDxES~v";
+    $url_adres_yandex_frame = "<iframe src=\"https://api-maps.yandex.ru/frame/v1/-/CZDxES~v\" width=\"560\" height=\"400\" frameborder=\"0\"></iframe>";
+    $url_oplata = "https://money.yandex.ru/to/410014778658914";
+    $push_impulse = "";
+}
+
+if (@$town == 'ufa') {
+    $main_name = "Ufa.".$main_name;
+    $site_name = $main_name;
+    $password = '111qwaszx';
+    $site_url = "http://".$main_name."/";
+    $site_phone = "+7 (960) 590 95 72";
+    $email = "zakaz@".$main_name;
+    $email_1 = "advertisement@".$main_name;
+    $email_2 = "advertising@".$main_name;
+    $email_3 = "promotion@".$main_name;
+    $email_4 = "advt@".$main_name;
+    $email_5 = "adt@".$main_name;
+    $email_6 = "zakaz@".$main_name;
+    $email_7 = "info@".$main_name;
+    $email_8 = "inform@".$main_name;
+    $email_9 = "information@".$main_name;
+    $email_10 = "order@".$main_name;
+    $email_11 = "ad@".$main_name;
+    $email_12 = "publicity@".$main_name;
+    $email2 = $main_name."@gmail.com";
+    $email3 = "info@".$main_name;
+    $adres = "450001, г. Уфа, Пролетарская ул, <br>дом № 40, oф. 89, ИНН 7813239461";
+    $url_adres_yandex = "https://yandex.ru/maps/-/CZDxE--I";
+    $url_adres_yandex_frame = "<iframe src=\"https://api-maps.yandex.ru/frame/v1/-/CZDxE--I\" width=\"560\" height=\"400\" frameborder=\"0\"></iframe>";
+    $url_oplata = "https://money.yandex.ru/to/410014778658914";
+    $push_impulse = "";
+}
+
+/*
+
 $url_town_piter = 'http://bazaznanij.com/';
 $url_town_moscow = 'http://moscow.bazaznanij.com/';
 $url_town_kazan = 'http://kazan.bazaznanij.com/';
@@ -7,6 +154,7 @@ $url_town_novosibirsk = 'http://novosibirsk.bazaznanij.com/';
 $url_town_ufa = 'http://ufa.bazaznanij.com/';
 
 //$town = 'moscow';
+
 
 $per = 2;
 if ($per == 1) {
@@ -165,7 +313,7 @@ if (@$town == 'ufa') {
 
 
 
-/*
+
 if ($per == 1) {
     $site_name = "<a href='http://vivadiplom.ru'>Да здравствует Диплом!</a>";
     $site_url = "http://vivadiplom.ru";
