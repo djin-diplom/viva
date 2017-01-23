d35b3f16ef317accf98d0add9e32d6f808d0a35d
21vek <git@github.com>
2017-01-23 19:13:21 +0300

    1

--- vivadiplomfirstallemail/email.php
+++ vivadiplomfirstallemail/email.php
@@ -195,7 +195,9 @@ $path_img = $site_url."vivadiplomfirstallemail/";
 			Номер заказа <?php echo $zakaz; ?><br>
 			Логин для входа в Личный кабинет (и ссылка): <a style="text-decoration:none;color:#5298ef" href="<?php echo $url_cabinet."?client_email=".$client_email."&"."client_pass=".$client_pass."&kabinet=1"; ?>" target="_blank" data-saferedirecturl="<?php echo $url_cabinet; ?>"><?php echo $client_email; ?></a><br>
 			Запомните Ваш пароль: <?php echo $client_pass; ?><br>
-			<?php if( $predoplata == 0) echo "<span style='color:red;'>Заказ без предоплаты с наценкой 30%!</span>";?>
+			<?php if( $predoplata == 0) echo "<span style='color:red;'>Заказ без предоплаты с наценкой 30%!</span><br>
+Вы можете снизить стоимость заказа, внеся предоплату!<br>
+Для начала работ требуется подтвердить заказ в ответе на письмо.";?>
 		</td>
 	</tr>
 	<tr>
