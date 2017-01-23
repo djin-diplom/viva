f0d820c31245325c2c0c5beb7a1ddbf0bb2a5ced
21vek <git@github.com>
2017-01-23 13:26:05 +0300

    1

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 
 <div id="marquee">Сделайте заказ сейчас, воспользуйтесь формой заказа на сайте! У нас нет телефонных менеджеров, они увеличивают цену работы!</div>
 <div id="marquee2">У нас нет телефонных менеджеров, они увеличивают цену работы!</div>
-if ($pokazati_menu == 0) {echo "<noindex>";}
+<?php if ($pokazati_menu == 0) {echo "<noindex>";} ?>
 	<header>
-		if ($pokazati_menu == 1) {echo "<noindex>";}
+		<?php if ($pokazati_menu == 1) {echo "<noindex>";}?>
 		<section>
 			<div class="logo"><a href="index.php" ><img src="./index_files/1_logo.png" width="222" height="70" alt=""></a>
 				<br><span style="color:green; font-size:18px;">Напишу - <?php echo $site_name; ?>!</span></div>
@@ -49,7 +49,7 @@ if ($pokazati_menu == 0) {echo "<noindex>";}
 			<a href="<?php echo $url_town_ufa; ?>" class="jQtooltip" title="Уфа"><img src="./index_files/book.png" width="<?php if($town == 'ufa') echo '50'; else echo '40';?>" height="<?php if($town == 'ufa') echo '64'; else echo '56';?>" alt=""></a>
 		</div>
 
-		if ($pokazati_menu == 1) {echo "</noindex>";}
+		<?php if ($pokazati_menu == 1) {echo "</noindex>";}?>
 		<menu>
 			<div><span><a href="o_kompanii.php">Наша компания</a></span></div>
 			<div><span><a href="uslugi_i_ceny.php">Стоимость работ</a></span></div>
@@ -63,4 +63,4 @@ if ($pokazati_menu == 0) {echo "<noindex>";}
         </menu>
 		
 	</header>
-if ($pokazati_menu == 0) {echo "</noindex>";}
+<?php if ($pokazati_menu == 0) {echo "</noindex>";}?>
