6c3fd6032fd9d69fe32e5794bba754b611e666a4
21vek <git@github.com>
2017-01-25 23:21:34 +0300

    1

--- head_1.php
+++ head_1.php
@@ -3,8 +3,8 @@
 
 
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-<script src="../js/jquery-1.3.2.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="../js//jquery.fancybox-1.2.1.pack.js"></script>
+<script src="js/jquery-1.3.2.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/jquery.fancybox-1.2.1.pack.js"></script>
 <link rel="stylesheet" type="text/css" href="index_files/style.css">
 <script type="text/javascript" async="" src="../index_files/watch.js"></script>
 
