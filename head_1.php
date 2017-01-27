164ea0241dea2a69e687b2043600e62542b39aa5
21vek <git@github.com>
2017-01-27 23:37:14 +0300

    1

--- head_1.php
+++ head_1.php
@@ -1,6 +1,8 @@
 <?php session_start();
 ?>
-
+<meta name="yandex-verification" content="a1d4839128ca9e06" />
+<meta name="yandex-verification" content="f00b337cb1083d8e" />
+<meta name="yandex-verification" content="526e187ad8458260" />
 
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 <script src="js/jquery-1.3.2.min.js" type="text/javascript"></script>
