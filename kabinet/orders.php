c04cb59724139e33af14a2eff127e9c30719a5d0
21vek <git@github.com>
2017-01-29 13:15:13 +0300

    1

--- kabinet/orders.php
+++ kabinet/orders.php
@@ -216,21 +216,21 @@ require ("kab_header.php");
 
 			<div class="<?php if ($predoplata == 0) echo 'amount-line price-amount'; else echo 'full-price'; ?>">
 				<div class="amount-text">Цена без предоплаты</div>
-				<div class="<?php if ($predoplata == 0) echo 'amount column'; else echo 'amount'; ?>">
+				<div class="<?php if ($predoplata == 0) echo 'amount'; else echo 'amount column'; ?>">
 					<?php echo (int)$zakaz_cena; ?><span class="rouble">p</span>
 </div>
 				<div class="splitter"></div>
 			</div>
 	<div class="<?php if ($predoplata == 1) echo 'amount-line price-amount'; else echo 'full-price'; ?>">
 		<div class="amount-text">Цена с предоплатой 50%</div>
-		<div class="<?php if ($predoplata == 1) echo 'amount column'; else echo 'amount'; ?>">
+		<div class="<?php if ($predoplata == 1) echo 'amount'; else echo 'amount column'; ?>">
 			<?php echo (int)($zakaz_cena*0.9); ?><span class="rouble">p</span>
 		</div>
 		<div class="splitter"></div>
 	</div>
 	<div class="<?php if ($predoplata == 2) echo 'amount-line price-amount'; else echo 'full-price'; ?>">
 		<div class="amount-text">Цена с полной предоплатой</div>
-		<div class="<?php if ($predoplata == 2) echo 'amount column'; else echo 'amount'; ?>">
+		<div class="<?php if ($predoplata == 2) echo 'amount'; else echo 'amount column'; ?>">
 			<?php echo (int)($zakaz_cena*0.8); ?><span class="rouble">p</span>
 		</div>
 		<div class="splitter"></div>
