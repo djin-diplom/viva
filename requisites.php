2a4b06c78529088b80a55e023a10dc4df6967c5d
21vek <git@github.com>
2017-02-08 07:54:09 +0300

    1

--- requisites.php
+++ requisites.php
@@ -12,7 +12,7 @@ $url_town_ufa = 'http://ufa.'.$main_name.'/';
 $vremia_raboti = "С 9:00 до 20:00, выходной воскресенье";
 
     $site_name = 'Viva-students.ru';
-$site_name_2 = 'www.Viva-students.ru';
+$site_name_2 = 'www.viva-students.ru';
 $site_name_3 = 'Вива-Студентс-ру';
 $site_name_4 = 'Вива-Студентс.ру';
 $site_name_5 = 'Viva-students.ru';
