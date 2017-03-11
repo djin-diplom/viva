b09443a0527f7c62cb3c8db75bd18d9652b72173
21vek <git@github.com>
2017-03-11 20:30:43 +0300

    1

--- index.php
+++ index.php
@@ -27,52 +27,52 @@ $count_text = 31;
 
 		<div class="big"><label onclick="document.getElementById(&#39;shorttext&#39;).className=document.getElementById(&#39;shorttext&#39;).className==&#39;hidden&#39;?&#39;showed&#39;:&#39;hidden&#39;; document.getElementById(&#39;arr1&#39;).src=document.getElementById(&#39;arr1&#39;).getAttribute(&#39;src&#39;)==&#39;index_files/arr1.png&#39;?&#39;index_files/arr2.png&#39;:&#39;index_files/arr1.png&#39;;">
 				Студенческие работы на заказ</label><br><img id="arr1" class="desaturated" src="./index_files/arr1.png" width="64" height="9" alt="" onclick="document.getElementById(&#39;shorttext&#39;).className=document.getElementById(&#39;shorttext&#39;).className==&#39;hidden&#39;?&#39;showed&#39;:&#39;hidden&#39;; this.src=this.getAttribute(&#39;src&#39;)==&#39;index_files/arr1.png&#39;?&#39;index_files/arr2.png&#39;:&#39;index_files/arr1.png&#39;;"></div>
-		<div id="shorttext" class="hidden"><h1>Уникальные дипломные работы на заказ</h1>
-<div class="dashedborder">Наши клиенты знают, что работа на заказ в учебном центре «<?php echo $site_name; ?>» будет написана качественно!</div>
-<div class="dashedborder">Дипломные работы «под ключ» - написание и сопровождение до защиты.</div>
-<div class="dashedborder">Мы гарантируем: дипломная работа, написанная нашими авторами, будет безупречной.</div>
-<div class="dashedborder">Написание дипломных работ на заказ – это наш бизнес!</div>
-<div class="dashedborder">Мы сделали для Заказчиков удобный личный кабинет!</div>
-<div class="dashedborder">Мы всегда согласимся с мнением вашего научного руководителя.<br>
-	Сотрудничество с нами продолжится и после того, как вы получите от нас готовое дипломное исследование.<br>
-Подойти в офис в Москве, Санкт-Петербурге сделать заказ диплома по почте и через форму сайта.</div></div>
+		<div id="shorttext" class="hidden"><h1>Работа на заказ</h1>
+			<div class="dashedborder">Наши клиенты знают, что дипломная работа на заказ в «<?php echo $site_name; ?>» будет написана качественно и быстро.</div>
+			<div class="dashedborder">Написание дипломной работы начинается с составления плана. После утверждения плана мы выполняем диплом по частям: введение, главы диплома, заключение, приложения и список литературы. По мере написания работы Вы получаете работу по частям для проверки и согласования с научным руководителем.</div>
+			<div class="dashedborder">Уже готовый диплом оформляется по методичке и Вам останется прийти на защиту и получить отличную оценку!</div>
+			<div class="dashedborder">Для защиты диплома мы подготовим доклад и презентацию бесплатно.</div>
+			<div class="dashedborder">Мы выполняем дипломные работы для всех специальностей и почти на любые темы.<br>
+				Можно написать диплом на примере Вашей организации со всеми данными.</div>
+			<div class="dashedborder">Дипломная или курсовая работа в 2016 году - это довольно непросто, особенно из-за требований по антиплагиату.<br>
+				Если Вам нужно заказать диплом или курсовую, то лучшее решение - это обратиться в компанию «<?php echo $site_name; ?>».<br>
+				Подойти в офис в Москве или сделать заказ диплома по почте и через сайт.</div></div>
 
 		<menu class="advantages">
-			<span class="except"><b>Наши достоинства:</b></span>
+			<span class="except"><b>Наши преимущества:</b></span>
 
-			
-<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,1);">Антиплагиaт</label></span>
-<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,2);">Качествo</label></span>
-<span class="active"><label onclick="changeSliderFrame(&#39;slider1&#39;,3);">Оперативнoсть</label></span><wbr>
-<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,4);">Готовые примеры</label></span>
-<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,5);">Гарантии</label></span>
-<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,6);">Оферта</label></span>
+
+			<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,1);">Антиплагиат</label></span>
+			<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,2);">Качество</label></span>
+			<span class="active"><label onclick="changeSliderFrame(&#39;slider1&#39;,3);">Оперативность</label></span><wbr>
+			<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,4);">Готовые кейсы</label></span>
+			<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,5);">Гарантия</label></span>
+			<span class=""><label onclick="changeSliderFrame(&#39;slider1&#39;,6);">Договор</label></span>
 
 		</menu>
 		<section class="slider" id="slider1" data-frames="#slider1 &gt; div &gt; div" data-delay="10000">
 			<span class="prev arrow"><img class="desaturated" src="./index_files/arr3.png" width="8" height="23" alt="" onclick="prevSliderFrame(&#39;slider1&#39;);"></span>
 				<div>
-			
-<div style="opacity: 0; z-index: 0; background: url(index_files/slider1.jpg) left center no-repeat;">
-	<div style="width: 480px;"></div>
-	<div><br><h3>У нас работают преподаватели вузов, знающие о дипломах всё, что только возможно знать.</h3>
-		<h3>Начиная с ГОСТов оформления и заканчивая изысками психологии членов аттестационной комиссии.</h3></div>
-</div>
+
+					<div style="opacity: 0; z-index: 0; background: url(index_files/slider1.jpg) left center no-repeat;">
+						<div style="width: 480px;"></div>
+						<div><br><h3>Вашу работу выполняют лучшие профессионалы</h3><h3>В работах нет плагиата из Интернета</h3></div>
+					</div>
 					<div style="background: url(index_files/slider2.jpg) right top no-repeat; z-index: 1; opacity: 0;">
-	<div><br><h3>Ваша дипломная работа будет оформлена<br>в чётком соответствии с требованиями вуза.</h3><h3>Большинствo сдаёт на «отличнo»</h3></div>
-</div><div style="background: url(index_files/slider3.jpg) left top no-repeat; z-index: 2; opacity: 1;">
-	<div style="width: 329px;"></div>
-	<div><br><h3>Все работы — точно в срок</h3></div>
-</div><div style="background: url(index_files/slider4.jpg) right top no-repeat; z-index: 0;">
-	<div><br><h3><a href="gotovie_keisy.php">Индивидуальный подход к каждому клиенту</a></h3></div>
-	<div style="width: 433px;"></div>
-</div><div style="background: url(index_files/slider5.jpg) left top no-repeat; z-index: 0;">
-	<div style="width: 329px;"></div>
-	<div><br><h3>Бесплатные доработки</h3><h3>Конфиденциальность гарантирована</h3></div>
-</div><div style="background: url(index_files/slider6.jpg) right top no-repeat; z-index: 0;">
-	<div><br><h3>Мы работаем по публичному договору оферты</h3><h3>Мы являемся официальнo зарегистрированнoй компанией</h3></div>
-	<div style="width: 354px;"></div>
-</div>
+						<div><br><h3>Выполнение всех требований и инструкций</h3><h3>Большинство сдаёт на «отлично»</h3></div>
+					</div><div style="background: url(index_files/slider3.jpg) left top no-repeat; z-index: 2; opacity: 1;">
+						<div style="width: 329px;"></div>
+						<div><br><h3>Все работы — точно в срок</h3></div>
+					</div><div style="background: url(index_files/slider4.jpg) right top no-repeat; z-index: 0;">
+						<div><br><h3><a href="gotovie_keisy.php">Индивидуальный подход к каждому клиенту</a></h3></div>
+						<div style="width: 433px;"></div>
+					</div><div style="background: url(index_files/slider5.jpg) left top no-repeat; z-index: 0;">
+						<div style="width: 329px;"></div>
+						<div><br><h3>Бесплатные доработки</h3><h3>Конфиденциальность гарантирована</h3></div>
+					</div><div style="background: url(index_files/slider6.jpg) right top no-repeat; z-index: 0;">
+						<div><br><h3>При оформлении заказа составляется договор и бланк заказа</h3><h3>Мы являемся официально зарегистрированной организацией</h3></div>
+						<div style="width: 354px;"></div>
+					</div>
 				</div>
 			<span class="next arrow"><img class="desaturated" src="./index_files/arr4.png" width="8" height="23" alt="" onclick="nextSliderFrame(&#39;slider1&#39;);"></span>
 		</section>
