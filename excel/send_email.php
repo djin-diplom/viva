f41acef002edc2663497566d0804fb306e15ae2a
21vek <git@github.com>
2017-03-29 19:19:49 +0300

    1

--- excel/send_email.php
+++ excel/send_email.php
@@ -84,7 +84,7 @@ if ( (int)$i >= (int)$count) {
 
         $body = $body_2;
 
-        $subject =  $name . "! " . $vid_rab . " за " . $pay . " без предоплаты в учебном центре " . $site_name;
+        $subject =  $name . "! " . $vid_rab . " по цене " . $pay . ", нет предоплаты в центре " . $site_name;
 
         $sender = new sender($email, $site_name, $subject, '', $password);
 
