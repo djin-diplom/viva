4307bab78b65e399c1cf403b3db38952d7563da4
21vek <git@github.com>
2017-06-19 20:12:17 +0300

    1

--- aktcii_i_skidki.php
+++ aktcii_i_skidki.php
@@ -28,13 +28,13 @@ require("header.php");
 	<section class="main">
 		<div>
 			<button class="calc2" onclick="self.location=&#39;/order/&#39;;">Рассчитать стоимость</button>
-			<noindex>
+
 			<section class="blocks2" style="margin-top: 90px;">
 				<?php
 				require("blocks.php");
 				?>
 			</section>
-			</noindex>
+			
 		</div>
 		<div>
 
