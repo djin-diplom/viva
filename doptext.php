8ae8cd626a59794e1b630f8c91e6e8d84ec36c28
21vek <git@github.com>
2017-09-09 09:27:23 +0300

    1

---
+++ doptext.php
@@ -0,0 +1,18 @@
+<h3>Пример работы <?php echo $name_rab_text." № ".$count_text; ?></h3>
+<?php
+$f = fopen("texts/new_" . $count_text, "r");
+$col_simv = 0;
+while (!feof($f)) {
+    $fgets = fgets($f);
+    $col = iconv_strlen($fgets);
+    $col_simv = $col_simv + $col;
+    if ($col > 2) {
+        echo $fgets . "<br /><br />";
+
+    }
+
+}
+fclose($f);
+$count_text = $count_text + 1;
+?>
+</div>
