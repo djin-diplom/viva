0a2e5b29bed17a30241c7ff202ebcf6ca7cc2b60
21vek <git@github.com>
2017-09-27 08:42:13 +0300

    1

--- email/build.php
+++ email/build.php
@@ -3,9 +3,7 @@ require("../requisites.php");
 $name = "Сергей";
 $vid_rab = "Курсовая";
 $pay = "2760";
-$predmet = 'Алгебра и Геометрия';
-
-if ($predmet != '') $predmet = $predmet.'.';
+$predmet = '';//'Алгебра и Геометрия';
 
 //$name = "Mosdiplpom.ru";
 //$vid_rab = "Магистерские и диссертации оптом";
