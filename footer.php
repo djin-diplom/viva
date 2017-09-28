d6fe35dc5e45892533f75994701771fcaa456ae2
21vek <git@github.com>
2017-09-28 20:22:20 +0300

    1

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <section>
-        <div class="copy"><div><span ondblclick="self.location=&#39;/indeego.php&#39;;">©</span> 1999 <?php echo $site_name; ?></div></div>
+        <div class="copy"><div><span ondblclick="self.location=&#39;/indeego.php&#39;;">©</span> 2017 <?php echo $site_name; ?></div></div>
         <div class="addr"><b><?php echo $adres; ?></b><br><?php echo $vremia_raboti; ?><div><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></div></div>
         <div class="links"><a href="articles.php">Пoлезные статьи</a><br><a href="gotovie_keisy.php">Готовые примеры</a><br><a href="job_creators.php">Работа авторам</a></div>
     </section>
@@ -11,9 +11,9 @@
     <h2>Акция!</h2>
     <h2>Цeны ниже</h2>
     <h3>на 20%!</h3>
-    <h4>Магистерская=7000</h4><br>
-    <h4>Диплом=4500</h4><br>
-    <h4>Курсовaя=1700</h4>
+    <h4>Магистерская=9000</h4><br>
+    <h4>Диплом=6500</h4><br>
+    <h4>Курсовaя=2000</h4>
     <h2>(Скидка 20% при полной предoплате)</h2>
     <a href="order.php">Сделать зaказ</a>
 
