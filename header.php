6d707a81d5383c5c1a327ec971d5f64b791bc7a3
21vek <git@github.com>
2018-01-05 15:18:03 +0300

    1

--- header.php
+++ header.php
@@ -63,7 +63,7 @@
 
 		<menu>
 			<div><span><a href="o_kompanii.php">Наша история</a></span></div>
-			<div><span><a href="uslugi_i_ceny.php">Наши цены</a></span></div>
+			<div><span><a href="uslugi_i_ceny.php">Цены</a></span></div>
 			<div><span><a href="poriadok_zakaza.php">Что делать?</a></span></div>
 			<div><span><a href="order.php">Сделать заказ</a></span></div>
 			<div><span><a href="consult.php">Отзывы</a></span></div>
