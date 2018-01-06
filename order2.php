62d48e969cf9334bdb04fb1b00109904016af02c
21vek <git@github.com>
2018-01-06 23:55:10 +0300

    1

--- order2.php
+++ order2.php
@@ -31,100 +31,100 @@ require("header.php");
 		?>
 		<div>
 
-<?php
-		//Если форма отправлена
-if(isset($_POST['submit'])) {
+			<?php
+			//Если форма отправлена
+			if(isset($_POST['submit'])) {
 
 
-	//Проверка суммы
-	//if(trim($_POST['count_1']) != $_POST['count_2']) {
-	//	$hasError = true;
-	//}
-	//Проверка стоимости от Заказчика
-	if(trim($_POST['kli_stoimost']) == '') {
-		$kli_stoimost = 0;
-	} else {
-		$kli_stoimost = trim($_POST['kli_stoimost']);
-	}
-	//Проверка предоплаты
-	if(trim($_POST['predoplata']) == '') {
-		$predoplata = 2;
-	} else {
-		$predoplata = trim($_POST['predoplata']);
-	}
-	//Срок выполнения
-	if(trim($_POST['srok_vip']) == '') {
-		$srok_vip= "Произвольный";
-	} else {
-		$srok_vip= $_POST['srok_vip'];
-	}
-	//Объем
-	if(trim($_POST['objem']) == '') {
-		$objem= "Произвольный";
-	} else {
-		$objem= $_POST['objem'];
-	}
-	//Уникальность
-	if(trim($_POST['unik']) == '') {
-		$unik= "Стандартная";
-	} else {
-		$unik= $_POST['unik'];
-	}
-	//Проверка тип работы
-	if(trim($_POST['work_kind']) == '') {
-		$hasError = true;
-	} else {
-		$work_kind = trim($_POST['work_kind']);
-	}
-	//Проверка поля email
-	if(trim($_POST['email_stud']) == '') {
-		$hasError = true;
-	} else {
-		$email_stud= $_POST['email_stud'];
-	}
-	if(trim($_POST['name']) == '') {
-		$name= $_POST['email_stud'];
-	} else {
-		$name= $_POST['name'];
-	}
-	if(trim($_POST['tema']) == '') {
-		$hasError = true;
-	} else {
-		$tema= $_POST['tema'];
-		$tema = str_replace('"','', $tema);
-		$tema = str_replace('\n',' ', $tema);
-	}
-	if(trim($_POST['predmet']) == '') {
-		$predmet= "Нет";
-	} else {
-		$predmet= $_POST['predmet'];
-	}
-	if(trim($_POST['university']) == '') {
-		$university= "Нет";
-	} else {
-		$university= $_POST['university'];
-	}
-	if(trim($_POST['add_info']) == '') {
-		$add_info= "Нет";
-	} else {
-		$add_info= $_POST['add_info'];
-		$add_info = str_replace('"','', $add_info);
-		$add_info = str_replace('\n',' ', $add_info);
-	}
+				//Проверка суммы
+				//if(trim($_POST['count_1']) != $_POST['count_2']) {
+				//	$hasError = true;
+				//}
+				//Проверка стоимости от Заказчика
+				if(trim($_POST['kli_stoimost']) == '') {
+					$kli_stoimost = 0;
+				} else {
+					$kli_stoimost = trim($_POST['kli_stoimost']);
+				}
+				//Проверка предоплаты
+				if(trim($_POST['predoplata']) == '') {
+					$predoplata = 2;
+				} else {
+					$predoplata = trim($_POST['predoplata']);
+				}
+				//Срок выполнения
+				if(trim($_POST['srok_vip']) == '') {
+					$srok_vip= "Произвольный";
+				} else {
+					$srok_vip= $_POST['srok_vip'];
+				}
+				//Объем
+				if(trim($_POST['objem']) == '') {
+					$objem= "Произвольный";
+				} else {
+					$objem= $_POST['objem'];
+				}
+				//Уникальность
+				if(trim($_POST['unik']) == '') {
+					$unik= "Стандартная";
+				} else {
+					$unik= $_POST['unik'];
+				}
+				//Проверка тип работы
+				if(trim($_POST['work_kind']) == '') {
+					$hasError = true;
+				} else {
+					$work_kind = trim($_POST['work_kind']);
+				}
+				//Проверка поля email
+				if(trim($_POST['email_stud']) == '') {
+					$hasError = true;
+				} else {
+					$email_stud= $_POST['email_stud'];
+				}
+				if(trim($_POST['name']) == '') {
+					$name= $_POST['email_stud'];
+				} else {
+					$name= $_POST['name'];
+				}
+				if(trim($_POST['tema']) == '') {
+					$hasError = true;
+				} else {
+					$tema= $_POST['tema'];
+					$tema = str_replace('"','', $tema);
+					$tema = str_replace('\n',' ', $tema);
+				}
+				if(trim($_POST['predmet']) == '') {
+					$predmet= "Нет";
+				} else {
+					$predmet= $_POST['predmet'];
+				}
+				if(trim($_POST['university']) == '') {
+					$university= "Нет";
+				} else {
+					$university= $_POST['university'];
+				}
+				if(trim($_POST['add_info']) == '') {
+					$add_info= "Нет";
+				} else {
+					$add_info= $_POST['add_info'];
+					$add_info = str_replace('"','', $add_info);
+					$add_info = str_replace('\n',' ', $add_info);
+				}
 
 
-	//Если ошибок нет, отправить email
-	if (!isset($hasError)) {
-		//$body = "
-		//name: $name \n\n
-		//email_stud: $email_stud \n\n
-		//work_kind: $work_kind \n\n
-		//tema: $tema \n\n
-		//predmet : $predmet \n\n
-		//university: $university \n\n
-		//add_info: $add_info \n\n
-		//";
-		$body = "
+				//Если ошибок нет, отправить email
+				if (!isset($hasError)) {
+					//$body = "
+					//name: $name \n\n
+					//email_stud: $email_stud \n\n
+					//work_kind: $work_kind \n\n
+					//tema: $tema \n\n
+					//predmet : $predmet \n\n
+					//university: $university \n\n
+					//add_info: $add_info \n\n
+					//";
+					$body = "
 		<html>
 <head>
   <title>Birthday Reminders for August</title>
@@ -147,27 +147,27 @@ if(isset($_POST['submit'])) {
 </html>
 		";
 
-		$headers  = 'MIME-Version: 1.0' . "\r\n";
-		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-		$headers .= 'From: '.$email. "\r\n" . 'Reply-To: ' . $email_stud;
-		$subject = "Письмо от клиента ".$name;
+					$headers  = 'MIME-Version: 1.0' . "\r\n";
+					$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+					$headers .= 'From: '.$email. "\r\n" . 'Reply-To: ' . $email_stud;
+					$subject = "Письмо от клиента ".$name;
 
-		mail($email2, $subject, $body, $headers);
-		$emailSent = true;
+					mail($email2, $subject, $body, $headers);
+					$emailSent = true;
 
-	}
-	include("win.php");
-}
+				}
+				include("win.php");
+			}
 
-?>
+			?>
 		</div>
 	</section>
 </div>
-			<section class="offers"><div><a href="aktcii_i_skidki.php"><img src="./index_files/offer1.gif" width="805" height="189" alt=""></a></div></section>
-			<?php
-			require("responses.php");
-			require("payment.php");
-			require("footer.php");
-			?>
+<section class="offers"><div><a href="aktcii_i_skidki.php"><img src="./index_files/offer1.gif" width="805" height="189" alt=""></a></div></section>
+<?php
+require("responses.php");
+require("payment.php");
+require("footer.php");
+?>
 </body>
 </html>
