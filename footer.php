4c8b0a513c3f6993f48ba07e4e85d7ff820b8b2c
21vek <git@github.com>
2018-01-07 00:36:34 +0300

    1

--- footer.php
+++ footer.php
@@ -1,13 +1,17 @@
-<!--noindex-->
+
 <footer>
     <section>
+        <!--noindex-->
         <div class="copy"><div><span ondblclick="self.location=&#39;/indeego.php&#39;;">©</span> 2018 <?php echo $site_name; ?></div></div>
         <div class="addr"><b><?php echo $adres; ?></b><br><?php echo $vremia_raboti; ?><div><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></div></div>
         <div class="links"><a href="articles.php">Пoлезные статьи</a><br><a href="gotovie_keisy.php">Готовые примеры</a><br><a href="job_creators.php">Работа авторам</a></div>
+        <!--/noindex-->
     </section>
 </footer>
 
+
 <div id="slidebox1">
+    <!--noindex-->
     <a class="close">x</a>
     <h2>Срочность!</h2>
     <h2>Без предоплат!</h2>
@@ -16,6 +20,5 @@
     <h4>Дипломная работа за 5000 рублей.</h4>
     <h2>Курсовая за 1500 рублей</h2>
     <a href="order.php">Сделать заказ</a>
-
+    <!--/noindex-->
 </div>
-<!--/noindex-->
--- payment.php
+++ payment.php
@@ -3,10 +3,7 @@
     <!--noindex-->
     <div>Оплата заказа возможна <br><a href="oplata.php">наличными в офисах центра</a>, другие варианты:</div>
     <!--/noindex-->
-    <!--
-    <div><a href="/oplata/#visa"><img class=desaturated src="/img/pay1.gif" width=78 height=37 alt=""></a></div>
-    <div><a href="/oplata/#mastercard"><img class=desaturated src="/img/pay2.gif" width=62 height=37 alt=""></a></div>
-    -->
+    
 
     <div><a href="oplata.php"><img class="desaturated" src="<?php echo $img_path_1; ?>" width="122" height="37" alt=""></a></div> <!-- width="122" height="37" -->
     <div><a href="oplata.php"><img class="desaturated" src="./index_files/pay3.gif" width="85" height="37" alt=""></a></div>
